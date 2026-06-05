42fec7fdf7001604992fb709bef1133bf48c3b94
Emanuel Henrique <emanuelmacedo@alunos.utfpr.edu.br>
2026-06-05 08:57:25 -0300

    Remove unnecessary blank lines in test function

--- red.c
+++ red.c
@@ -12,11 +12,9 @@ void testTwoSumEncontrarAlvo18()
 {
     int nums[] = {2, 7, 11, 15};
     int indices[2];
-    
     twoSum(nums, 4, 18, indices);
     assert(indices[0] == 1); 
     assert(indices[1] == 2);
-    
     printf("Teste 1: Sucesso!\n");
 }
 
